cc52c16436892c737d2f2ff5534cb5d95775fdae
priyanshp2211 <priyansh.london@gmail.com>
2021-06-19 23:16:09 +0100

    hard push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './nicepage.css';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavourites';
